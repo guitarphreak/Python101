03c130100b7f8d69d6a418e0721c5372e66332d2
Andres Quiroz <guitarphreak@gmail.com>
2013-10-22 00:37:55 -0400

    Create locators - WIP
    Still needs more polishing, better UI to input [x,y,z] coords and more
    functionality.

--- layout/arm_lyt.py
+++ layout/arm_lyt.py
@@ -7,12 +7,10 @@ if result=='OK':
     i = 0
     while (i<numLocators):
         i += 1
-        mc.spaceLocator(p=(0,0,0), n='locatorDelDemonio%d' %i)
+        mc.spaceLocator(position=(0,0,0), name='locatorDelDemonio%d' %i)
         
 else:
     print 'Script cancelled by user'
-    return
 
-if int(numLocators = int(mc.promptDialog(query=True, text=True)))<1:
+if int(mc.promptDialog(query=True, text=True))<1:
     print 'Is not possible to create less than one locator!'
-    return
